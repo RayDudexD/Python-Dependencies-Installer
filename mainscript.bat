6e247746ee89a6207e5f9dab5198283554e06504
RayDudelol <raydudexd@gmail.com>
2025-01-04 15:19:47 +0100

    updating to the definitive edition 💯
    deleted the check cause it was either broken or it was useless.
    the script is now so short.
    this is the definitive edition of this program!

--- mainscript.bat
+++ mainscript.bat
@@ -1,47 +1,12 @@
 @echo off
-echo Welcome to Python Dependency Downloader. Happy new year!
-goto :CheckPython
+goto :mainmenu
 
-:CheckPython
-echo Checking for Python installation...
-for %%i in (python python3) do (
-    where %%i >nul 2>nul
-    if not errorlevel 1 (
-        goto :PythonInstalled
-    )
-)
-
-goto :InstallPython
-
-:InstallPython
+:mainmenu
 cls
-echo Python isn't detected.
-echo 1) Microsoft Version.
-echo 2) Python Website version.
-choice /c 12 /n /m "Choose your number:"
-if errorlevel 2 goto :Pyth
-if errorlevel 1 goto :Micro
-pause
-goto :CheckPython
-
-:Micro
-start "" "https://apps.microsoft.com/detail/9NRWMJP3717K?hl=en-US&gl=US"
-echo When you are done installing, press any key to check again!
-pause
-goto :CheckPython
-
-:Pyth
-start "" "https://www.python.org/downloads/"
-echo When you are done installing, press any key to check again!
-pause
-goto :CheckPython
-
-:PythonInstalled
-cls
-echo Python is installed.
-echo 1) Install a dependency
-echo 2) Uninstall a dependency
-echo 3) Exit
+echo Welcome to the Python Dependency Downloader.
+echo 1) Install a dependency.
+echo 2) Uninstall a dependency.
+echo 3) Exit.
 choice /c 123 /n /m "Choose your number:"
 
 if errorlevel 3 goto :ExitScript
@@ -58,7 +23,7 @@ if errorlevel 1 (
     echo %input% installed successfully.
 )
 pause
-goto :PythonInstalled
+goto :mainmenu
 
 :uninstall
 set /p input=Enter the dependency name: 
@@ -70,8 +35,8 @@ if errorlevel 1 (
     echo %input% uninstalled successfully.
 )
 pause
-goto :PythonInstalled
+goto :mainmenu
 
 :ExitScript
-echo Exiting script. Goodbye!
+echo Exiting the script. Goodbye!
 exit /b 0
